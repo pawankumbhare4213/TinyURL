cd12c0c66e3af3e38c9b869b8a59c62332b1b12f
pawankumbhare4213 <pawankumbhare4213@gmail.com>
2017-05-05 05:12:15 -0700

    commit 2

--- src/com/tinyurl/constant/Constants.java
+++ src/com/tinyurl/constant/Constants.java
@@ -1,6 +1,9 @@
 package com.tinyurl.constant;
 
-public final class Constants {
+import java.io.InputStream;
+import java.util.Properties;
+
+public abstract class Constants {
 	public static final String URL_COUNTER = "URL_COUNTER";
 	public static final String URLS = "URLS";
 	public static final String HIT_COUNT = "HIT_COUNT";
@@ -9,12 +12,31 @@ public final class Constants {
 	public static final String TIMESTAMP = "TIMESTAMP";
 	public static final String INVALID_SHORT_URL = "Invalid URL!";
 	public static final String MESSAGE = "MESSAGE";
-	public static final String PRIVATE_KEY = "6LfsHSAUAAAAAIo3HkeBK3x66-9Jy7m5ld0_poRA";
-	public static final String IP = "74.208.169.217";
-	public static final int PORT = 3000;
-	public static final String NAMESPACE = "aerospike";
 	public static final String SUCCESS = "success";
 	public static final String SECRET = "secret";
 	public static final String RESPONSE = "response";
-	public static final String CAPTCHA_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
+	public static final String CONFIG_FILE = "com/tinyurl/conf/tinyurl.conf";
+
+	public static String PRIVATE_KEY = "PRIVATE_KEY";
+	public static String NAMESPACE = "NAMESPACE";
+	public static String CAPTCHA_VERIFY_URL = "CAPTCHA_VERIFY_URL";
+	public static String IP = "IP";
+	public static String PORT_NO = "PORT";
+	public static int PORT = 0;
+
+	static {		
+		Properties conf = new Properties();
+		try {
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			InputStream input = classLoader.getResourceAsStream(CONFIG_FILE);
+			conf.load(input);
+			PRIVATE_KEY = conf.getProperty(PRIVATE_KEY);
+			NAMESPACE = conf.getProperty(NAMESPACE);
+			CAPTCHA_VERIFY_URL = conf.getProperty(CAPTCHA_VERIFY_URL);
+			IP = conf.getProperty(IP);
+			PORT = Integer.parseInt(conf.getProperty(PORT_NO));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
